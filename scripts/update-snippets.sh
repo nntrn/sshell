8b608470ca5f7dc732f3c0686cc7cf69e6cf0900
Annie_Tran <17685332+nntrn@users.noreply.github.com>
2022-05-11 07:25:50 +0100

    Use git remote show origin to get remote branches

--- scripts/update-snippets.sh
+++ scripts/update-snippets.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+set -e
+
 DIR="$(cd "$(dirname "$0")/.." && pwd)"
+CURRENT_BRANCH=$(git branch --show-current)
+REMOTE_URL=$(git config --get remote.origin.url)
+GIT_BRANCH=data
+
 STDOUT="$@"
 
 if [[ -t 1 || $# -eq 0 ]]; then
   STDOUT=$(cat)
+fi
+
+if [[ $# -eq 1 && -f $1 ]]; then
+  STDOUT=$(cat $1)
 fi
 
 if [[ -z "$STDOUT" ]]; then
@@ -15,25 +25,23 @@ fi
 TMPDIR=$(mktemp -d)
 TMPFILE=$(mktemp)
 
-echo "$STDOUT" >$TMPFILE
-
-CURRENT_BRANCH=$(git branch --show-current)
-REMOTE_URL=$(git config --get remote.origin.url)
-GIT_BRANCH=data
+echo "$STDOUT" | jq >$TMPFILE
 
 cd $TMPDIR
 
 git init
 git remote add origin $REMOTE_URL
-git fetch -a
 
-if [[ $(git branch -a | grep $GIT_BRANCH) ]]; then
-  git checkout data
+REMOTEBRANCHES=$(git remote show origin | sed -n '/Remote branches/,/Local/p' | grep -v ':' | awk '{print $1}')
+
+if [[ $(echo "$REMOTEBRANCHES" | grep $GIT_BRANCH) ]]; then
+  git fetch origin $GIT_BRANCH
+  git checkout $GIT_BRANCH
 else
-  git checkout -b data
+  git checkout -b $GIT_BRANCH
 fi
 
-cat $TMPFILE >snippets.json
+cat $TMPFILE | jq >snippets.json
 
 if [[ -z $(git status --ignore-submodules=dirty | grep "nothing to commit, working directory clean") ]]; then
   git add snippets.json
