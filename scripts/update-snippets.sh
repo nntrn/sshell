5f0f41ae7dca21d7c6717ab91cd1ec59e4eecea5
Annie_Tran <annie.tran@dell.com>
2022-05-27 05:57:04 +0100

    Split snippets into files

--- scripts/update-snippets.sh
+++ scripts/update-snippets.sh
@@ -5,51 +5,76 @@ set -e
 DIR="$(cd "$(dirname "$0")/.." && pwd)"
 CURRENT_BRANCH=$(git branch --show-current)
 REMOTE_URL=$(git config --get remote.origin.url)
+REMOTE_BRANCHES=$(git remote show origin | sed -n '/Remote branches/,/Local/p' | grep -v ':' | awk '{print $1}')
 GIT_BRANCH=data
+TMPDIR=$(mktemp -d)
+TMPFILE=$TMPDIR/snippets-new.json
+TMPREADME=$TMPDIR/readme.md
+TMPMODFILE=$TMPDIR/modified.txt
+TODAY=$(date +%F)
+trap 'rm -rf -- "$TMPDIR"' EXIT
 
 STDOUT="$@"
 
-if [[ -t 1 || $# -eq 0 ]]; then
-  STDOUT=$(cat)
-fi
+source $DIR/scripts/utils.sh
 
-if [[ $# -eq 1 && -f $1 ]]; then
+if [[ -t 1 || $# -eq 0 || -f $1 ]]; then
   STDOUT=$(cat $1)
+  echo "$STDOUT" | python3 -m json.tool >$TMPFILE
+else
+  bye "An error occured reading stdin"
 fi
-
-if [[ -z "$STDOUT" ]]; then
-  echo "An error occured reading.. Aborting"
-  exit 1
-fi
-
-TMPDIR=$(mktemp -d)
-TMPFILE=$(mktemp)
-
-echo "$STDOUT" | jq >$TMPFILE
 
 cd $TMPDIR
 
 git init
 git remote add origin $REMOTE_URL
 
-REMOTEBRANCHES=$(git remote show origin | sed -n '/Remote branches/,/Local/p' | grep -v ':' | awk '{print $1}')
-
-if [[ $(echo "$REMOTEBRANCHES" | grep $GIT_BRANCH) ]]; then
+if [[ $(echo "$REMOTE_BRANCHES" | grep $GIT_BRANCH) ]]; then
   git fetch origin $GIT_BRANCH
   git checkout $GIT_BRANCH
 else
   git checkout -b $GIT_BRANCH
 fi
 
-cat $TMPFILE | jq >snippets.json
+[[ ! -d data ]] && mkdir data
 
-if [[ -z $(git status --ignore-submodules=dirty | grep "nothing to commit, working directory clean") ]]; then
-  git add snippets.json
-  git commit -m "Update changes"
+if [[ -f snippets.json ]]; then
+  export last_modified=$(cat snippets.json | get_last_modified)
+  export last_id=$(cat snippets.json | get_last_id)
+
+  cat $TMPFILE | jq -c "(.[]|select(.modified > \"$last_modified\"))" >$TMPMODFILE
+
+  if [[ -s $TMPMODFILE ]]; then
+    cat $TMPFILE >snippets.json
+    cat $TMPFILE | jq -c -r '.[]| .language' | sort | uniq -c | sort -n -r -k1 >$TMPREADME
+    languages=($(cat $TMPREADME | awk '{print $2}'))
+    cat $TMPREADME | awk '{print "- [" $2 "](#" $2 ") (" $1 ")" }'
+
+    while read -r line; do
+      filename="data/$(echo "$line" | jq -r '.id')"
+      echo "$line" | jq >$filename
+    done <$TMPMODFILE
+
+    for lang in "${languages[@]}"; do
+      echo -e "\n## $lang\n" >>$TMPREADME
+      cat $TMPFILE |
+        jq -r ".[] | select(.language==\"$lang\")| \"- [\"+.id+\"](data/\"+.id+\"): \"+.title" >>$TMPREADME
+    done
+
+    sed -i '1s/^/# data /' $TMPREADME
+    sed -i 's/  */ /g' $TMPREADME
+  fi
+fi
+
+if [[ -z $(git status snippets.json | grep "nothing to commit") ]]; then
+  git add snippets.json readme.md
+  git add data
+  git commit -m "Update data - $TODAY"
   git push origin $GIT_BRANCH
 else
   echo "No changes"
 fi
 
-[[ -f $TMPFILE ]] && rm -rf $TMPFILE
+cd $HOME
 [[ -d $TMPDIR ]] && rm -rf $TMPDIR
