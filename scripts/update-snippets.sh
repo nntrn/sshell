8c1a02c44b6dec1e811bb286d42ec610bd2eb053
Annie_Tran <17685332+nntrn@users.noreply.github.com>
2022-06-01 03:45:45 +0100

    Use git clone instead of init

--- scripts/update-snippets.sh
+++ scripts/update-snippets.sh
@@ -4,57 +4,48 @@ set -e
 
 STDOUT="$@"
 DIR="$(cd "$(dirname "$0")/.." && pwd)" && cd "$DIR"
-CURRENT_BRANCH=$(git branch --show-current)
 REMOTE_URL=$(git config --get remote.origin.url)
-REMOTE_BRANCHES=$(git remote show origin | sed -n '/Remote branches/,/Local/p' | grep -v ':' | awk '{print $1}')
 GIT_BRANCH=data
 TMPDIR=$(mktemp -d)
 TMPFILE=$TMPDIR/snippets-new.json
-TMPREADME=$TMPDIR/readme.md
+TMPREADME=readme.md
 TMPMODFILE=$TMPDIR/modified.txt
-TODAY=$(date +%F)
 
 trap 'rm -rf -- "$TMPDIR"' EXIT
 source $DIR/scripts/utils.sh
 
 if [[ -t 1 || $# -eq 0 || -f $1 ]]; then
   STDOUT=$(cat $1)
-  echo "$STDOUT" | python3 -m json.tool >$TMPFILE
+  echo "$STDOUT" >$TMPFILE
 else
   bye "An error occured reading stdin"
 fi
 
 cd $TMPDIR
 
-git init
-git remote add origin $REMOTE_URL
+git clone -b $GIT_BRANCH $REMOTE_URL && cd "$(basename "$_" .git)"
+git config --local user.email 17685332+nntrn@users.noreply.github.com
 
-if [[ $(echo "$REMOTE_BRANCHES" | grep $GIT_BRANCH) ]]; then
-  git fetch origin $GIT_BRANCH
-  git checkout $GIT_BRANCH
-else
-  git checkout -b $GIT_BRANCH
-fi
-
-[[ ! -d data ]] && mkdir data
-
-export last_modified=$(cat snippets.json | get_last_modified)
-export last_id=$(cat snippets.json | get_last_id)
-
+cp snippets.json{,.bkp}
+last_modified=$(get_last_modified <snippets.json)
 cat $TMPFILE | jq -c "(.[]|select(.modified > \"$last_modified\"))" >$TMPMODFILE
 cat $TMPFILE | jq -c -r '.[]| .language' | sort | uniq -c | sort -n -r -k1 >$TMPREADME
-cat $TMPFILE >snippets.json
+cat $TMPFILE | tr -d '\r' | tr -d '\0' >snippets.json
 languages=($(cat $TMPREADME | awk '{print $2}'))
 gawk -i inplace '{print "- [" $2 "](#" $2 ") (" $1 ")" }' $TMPREADME
 
 for lang in "${languages[@]}"; do
-  echo -e "\n## $lang\n" >>$TMPREADME
-  cat $TMPFILE |
-    jq -r ".[] | select(.language==\"$lang\")| \"- [\"+.id+\"](data/\"+.id+\"): \"+.title" >>$TMPREADME
-  echo -e "\n[[Return to top]](#data)\n" >>$TMPREADME
+  {
+    echo "## $lang"
+    jq -r ".[] | select(.language==\"$lang\")| \"- [\"+.id+\"](data/\"+.id+\"): \"+.title" <$TMPFILE
+    echo
+    echo "[[Return to top]](#data)"
+    echo
+  } >>$TMPREADME
 done
 
 if [[ -s $TMPMODFILE ]]; then
+  [[ ! -d data ]] && mkdir data
   while read -r line; do
     filename="data/$(echo "$line" | jq -r '.id')"
     echo "$line" | jq >$filename
@@ -64,9 +55,15 @@ fi
 sed -i '1s/^/# data\n\n /' $TMPREADME
 sed -i 's/  */ /g' $TMPREADME
 
-if [[ -z $(git status snippets.json readme.md data | grep "nothing to commit") ]]; then
+SNIPPETS_BKP_ID=$(jq -rc '.[]|.id' <snippets.json.bkp)
+SNIPPETS_ID=$(jq -rc '.[]|.id' <snippets.json)
+DELETED_IDS=($(grep -vFxf <(echo "$SNIPPETS_ID") <(echo "$SNIPPETS_BKP_ID") | sed 's,^,data/,g'))
+
+[[ ${#DELETED_IDS[@]} -gt 0 ]] && rm "${DELETED_IDS[@]}"
+
+if ! grep -q "nothing to commit" <(git status snippets.json readme.md data); then
   git add snippets.json readme.md data
-  git commit -m "Update data - $TODAY"
+  git commit -m "Update data"
   git push origin $GIT_BRANCH
 else
   echo "No changes"
