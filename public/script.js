8b92aa36f8743904b55c757450cc807b4f34ea93
Annie_Tran <17685332+nntrn@users.noreply.github.com>
2022-06-02 17:47:31 +0100

    Update static files in public

--- public/script.js
+++ public/script.js
@@ -1,9 +1,11 @@
+const $$ = query => Array.from(document.querySelectorAll(query))
+
 function getSnippetsUrl() {
-  return "https://raw.githubusercontent.com/nntrn/sshell/data/snippets.json"
+  return 'https://raw.githubusercontent.com/nntrn/sshell/data/snippets.json'
 }
 
 function removeHash() {
-  history.replaceState({}, document.title, window.location.href.split("#")[0])
+  history.replaceState({}, document.title, window.location.href.split('#')[0])
 }
 
 function groupByKey(obj, key) {
@@ -17,34 +19,34 @@ function groupByKey(obj, key) {
 
 function escapeHtml(s) {
   var ENTITY_MAP = {
-    "&": "&amp;",
-    "<": "&lt;",
-    ">": "&gt;",
-    '"': "&quot;",
-    "'": "&#39;",
-    "/": "&#x2F;",
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;',
+    '/': '&#x2F;',
   }
-  return ("" + s).replace(/[&<>"'/]/g, function (s) {
+  return ('' + s).replace(/[&<>"'/]/g, function (s) {
     return ENTITY_MAP[s]
   })
 }
 
 function buildSnippets(res) {
-  Object.entries(groupByKey(res, "language")).forEach((langGrp) => {
-    const group = Object.assign(document.createElement("div"), {
-      className: "group",
+  Object.entries(groupByKey(res, 'language')).forEach(langGrp => {
+    const group = Object.assign(document.createElement('div'), {
+      className: 'group',
       id: langGrp[0],
     })
 
     group.appendChild(
-      Object.assign(document.createElement("h3"), {
+      Object.assign(document.createElement('h3'), {
         textContent: langGrp[0],
       })
     )
 
-    document.querySelector("#toc").appendChild(
-      Object.assign(document.createElement("a"), {
-        className: "link",
+    document.querySelector('#toc').appendChild(
+      Object.assign(document.createElement('a'), {
+        className: 'link',
         href: `#${langGrp[0]}`,
         textContent: langGrp[0],
       })
@@ -52,37 +54,37 @@ function buildSnippets(res) {
 
     langGrp[1].forEach(({ output, ...e }) => {
       const itemjson = JSON.stringify(e, null, 2)
-      const codeText = escapeHtml([e.cli].flat(2).join("\n"))
+      const codeText = escapeHtml([e.cli].flat(2).join('\n'))
       group.appendChild(
-        Object.assign(document.createElement("div"), {
-          className: "item",
+        Object.assign(document.createElement('div'), {
+          className: 'item',
           id: `data-${e.id}`,
           innerHTML: [
             `<h4><a href="#${e.id}" onclick="showPopup(${e.id})">${e.title}</a></h4>`,
             `<div class="code" style="user-select:all">${codeText}</div>`,
             `<div class="filter" style="display:none">${itemjson}</div>`,
-          ].join("\n"),
+          ].join('\n'),
         })
       )
     })
-    document.querySelector("#sshell").appendChild(group)
+    document.querySelector('#sshell').appendChild(group)
   })
 }
 
 function searchItems(string) {
-  Array.from(document.querySelectorAll(".item .filter")).forEach((e) => {
+  Array.from(document.querySelectorAll('.item .filter')).forEach(e => {
     if (e.textContent.includes(string)) {
-      e.parentElement.classList.remove("hidden")
+      e.parentElement.classList.remove('hidden')
     } else {
-      e.parentElement.classList.add("hidden")
+      e.parentElement.classList.add('hidden')
     }
   })
 }
 
 function unhideAllItems(e) {
   console.log(e)
-  Array.from(document.querySelectorAll(".item .hidden")).forEach((e) => {
-    e.classList.remove("hidden")
+  Array.from(document.querySelectorAll('.item .hidden')).forEach(e => {
+    e.classList.remove('hidden')
   })
 }
 
@@ -100,8 +102,8 @@ function eventFilter(e) {
 }
 
 function hidePopup() {
-  document.querySelector("#popup").classList.remove("show")
-  Array.from(document.querySelectorAll("#popup [data-key]")).forEach((e) => (e.textContent = ""))
+  document.querySelector('#popup').classList.remove('show')
+  Array.from(document.querySelectorAll('#popup [data-key]')).forEach(e => (e.textContent = ''))
   removeHash()
 }
 
@@ -110,19 +112,22 @@ function getLocationId() {
 }
 
 function editPopupText(obj) {
-  Array.from(document.querySelectorAll("#popup [data-key]")).forEach(
-    (e) => (e.textContent = [obj[e.dataset.key]].flat(2).join("\n"))
-  )
+  $$('#popup [data-key]').forEach(e => {
+    if (e.nodeName.toLowerCase() === 'a') {
+      e.href = obj[e.dataset.key]
+    }
+    e.textContent = [obj[e.dataset.key]].flat(2).join('\n')
+  })
 }
 
 function showPopup(id = getLocationId()) {
   editPopupText(JSON.parse(document.querySelector(`#data-${id} .filter`).textContent))
-  document.querySelector("#popup").classList.add("show")
+  document.querySelector('#popup').classList.add('show')
 }
 
-document.querySelector("#searchbar").addEventListener("keyup", eventFilter)
+document.querySelector('#searchbar').addEventListener('keyup', eventFilter)
 
-document.querySelector("#searchbar").addEventListener("click", unhideAllItems)
+document.querySelector('#searchbar').addEventListener('click', unhideAllItems)
 
 document.onkeydown = function (evt) {
   if (evt.keyCode === 27) {
@@ -130,14 +135,14 @@ document.onkeydown = function (evt) {
   }
 }
 
-window.addEventListener("load", function () {
+window.addEventListener('load', function () {
   fetch(getSnippetsUrl())
-    .then((e) => e.json())
-    .then((res) => buildSnippets(res))
-    .then((e) => {
+    .then(e => e.json())
+    .then(res => buildSnippets(res))
+    .then(e => {
       if (location.hash) {
         showPopup()
       }
     })
-    .catch((err) => console.log(err))
+    .catch(err => console.log(err))
 })
