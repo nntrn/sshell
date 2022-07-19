ec393c6299fc809df34b642dc7f0025fbfa72a8d
annie tran <17685332+nntrn@users.noreply.github.com>
2022-07-19 06:31:58 -0500

    Update filename

--- build_static.py
+++ build_static.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from jinja2 import Environment, FileSystemLoader
 import json
 import os
@@ -15,7 +17,7 @@ snippets=json.load(sys.stdin)
 
 for snippet in snippets:
   all_snippets_html += code_tmpl.render(snippet)
-  filepath = 'public/api/' + snippet['id']
+  filepath = 'public/api/' + snippet['id'] + '.json'
   jsonstring=json.dumps(snippet,ensure_ascii=False,indent=2,sort_keys=False)
 
   with open(filepath, 'w') as f:
