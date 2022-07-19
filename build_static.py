28353cbca5854ac2cdae855f0281f5af6a8edeb4
annie tran <17685332+nntrn@users.noreply.github.com>
2022-07-19 07:10:56 -0500

    Add redirect

--- build_static.py
+++ build_static.py
@@ -8,6 +8,7 @@ import sys
 env = Environment(loader=FileSystemLoader('./templates'))
 code_tmpl = env.get_template('code_block.txt')
 html_tmpl = env.get_template('index.html.j2')
+redirect_tmpl = env.get_template('redirect.j2')
 
 all_snippets_html=''
 
@@ -17,11 +18,18 @@ snippets=json.load(sys.stdin)
 
 for snippet in snippets:
   all_snippets_html += code_tmpl.render(snippet)
-  filepath = 'public/api/' + snippet['id'] + '.json'
+  apipath = 'public/api/' + snippet['id']
+  filepath = apipath + '.json'
+
   jsonstring=json.dumps(snippet,ensure_ascii=False,indent=2,sort_keys=False)
 
-  with open(filepath, 'w') as f:
+  os.makedirs(apipath , exist_ok=True)
+
+  with open( apipath + '.json', 'w') as f:
     f.write(jsonstring)
+
+  with open(apipath + '/index.html', 'w') as f:
+    f.write(redirect_tmpl.render(id=snippet['id'],jsonstring=jsonstring))
 
 with open('public/index.html', 'w') as f:
   f.write(html_tmpl.render(snippets=all_snippets_html))
