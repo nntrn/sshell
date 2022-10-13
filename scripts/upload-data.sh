2b2e087a50e744a5d8480d8372e64f0a5ba6ff99
Annie_Tran <17685332+nntrn@users.noreply.github.com>
2022-10-13 04:50:22 +0100

    Update .gitignore

--- scripts/upload-data.sh
+++ scripts/upload-data.sh
@@ -1,26 +1,36 @@
 #!/usr/bin/env bash
 
-cd $(realpath ${0%/*})
+cd $(realpath ${0%/*}) || exit
 
-PARENTDIR="$(git rev-parse --show-toplevel)"
 REMOTE_URL=$(git config --get-regexp '^remote.*url' | awk '{print $2}' | head -n 1)
+REPO=$(basename ${REMOTE_URL%.git})
 BRANCH=data
-REPO=sshell
 
 TMPDIR=$(mktemp -d)
-cat $1 | tr -d '\r' >$TMPDIR/data.json
+cat | tr -d '\r' >$TMPDIR/data.json
 cat $TMPDIR/data.json | jq -rc '.[]' >$TMPDIR/data.txt
 
-cd $TMPDIR
-git clone -b $BRANCH $REMOTE_URL &>/dev/null
-cd $REPO
+cd $TMPDIR || exit
+git clone -b $BRANCH $REMOTE_URL $REPO &>/dev/null
+cd $REPO || exit
+
+if [[ $1 == all ]]; then
+  rm -rf $TMPDIR/$REPO/data
+fi
+
+[[ ! -d $TMPDIR/$REPO/data ]] && mkdir -p $TMPDIR/$REPO/data
 
 while read -r line; do
   SNIPPET_ID=$(echo "$line" | jq -r '.id')
-  echo "$line" | jq >data/$SNIPPET_ID
+  echo "$line" | jq >$TMPDIR/$REPO/data/$SNIPPET_ID
 done <$TMPDIR/data.txt
 
-cat $TMPDIR/data.json >$TMPDIR/$REPO/data.json
+cat $TMPDIR/$REPO/data/* | jq -s '.|sort_by(.id)|reverse' >$TMPDIR/$REPO/data.json
+
+echo "# data" >$TMPDIR/$REPO/readme.md
+cat $TMPDIR/$REPO/data.json | jq -r 'group_by(.language)|map({"\(.[0].language)": (.)}) |add|to_entries|.[]| ["\n## " +.key+"\n","- "+(.value|.[]|"[\(.id)](data/\(.id)): \(.title)")]|join("\n")' >>$TMPDIR/$REPO/readme.md
+
+jq -r 'sort_by(.modified)|.[-1]|{"last_modified":.modified}' $TMPDIR/$REPO/data.json >$TMPDIR/$REPO/meta.json
 
 git config --local --list
 git add -A .
